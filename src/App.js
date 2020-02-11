f5c7574fbcfa068b7020d68a1341e8c136db7887
courage173 <courageosemwengie@gmail.com>
2020-02-11 19:42:15 +0100

    feature(implemented the slider)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './resources/styles.css'
 import Header from './components/header_footer/Header'
+import Featured from './components/featured/index'
 
 
 function App() {
   return (
     <div className="App" style={{height:"1500px", background: "cornflowerblue"}}>
       <Header />
+      <Featured /> 
     </div>
   );
 }
