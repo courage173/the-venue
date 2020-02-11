1b84af0e1968a8cc66a66e2eefa64288afc9806c
courage173 <courageosemwengie@gmail.com>
2020-02-11 12:35:29 +0100

    feature(implemented nav bar)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './resources/styles.css'
+import Header from './components/header_footer/Header'
+
+
+function App() {
+  return (
+    <div className="App" style={{height:"1500px", background: "cornflowerblue"}}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
